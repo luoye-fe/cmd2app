c11a4d7b4a4d0b130eee77f5e6b22807bd929df2
luoye <842891024@qq.com>
2016-10-11 10:52:11 +0800

    update welcome component

--- static/components/welcome.vue
+++ static/components/welcome.vue
@@ -6,12 +6,8 @@
 		<h1>welcome</h1>
 		<p>这是由 <a href="javascript:void(0)" @click="openUrl('https://github.com/luoye-fe/cmd2app')"><code>cmd2app</code></a> 自动生成的命令行 GUI 程序。</p>
 		<p>开始使用之前，让我们先了解下一段命令行的构成。</p>
-		<pre>
-			<code>
-cmd2app new gomeplusFED/meixin-h5-proxy ./temp
-			</code>
-		</pre>
-
+		<pre><code></code>{{code}}</pre>
+		<p>根据上面的命令行构成，我们提供简洁的 GUI 界面可以得出所需的结果，并记录到历史中，方便下次调用。</p>
 		<span class="tips">此提示在打开过由  <a href="javascript:void(0)" @click="openUrl('https://github.com/luoye-fe/cmd2app')"><code>cmd2app</code></a> 生成的应用将不再展示。</span>
 	</div>
 </template>
@@ -25,6 +21,8 @@ cmd2app new gomeplusFED/meixin-h5-proxy ./temp
 	left: 0;
 	overflow: auto;
 	background-color: rgba(255, 255, 255, 1);
+	padding: 0 15px;
+	box-sizing: border-box;
 }
 
 .welcome .close {
@@ -54,7 +52,17 @@ export default {
 	name: 'Welcome',
 	data() {
 		return {
-			isFirst: window.localStorage.getItem('cmd2app-welcome')
+			isFirst: window.localStorage.getItem('cmd2app-welcome'),
+			code: 
+`cmd2app --port=8001 new gomeplusFED/meixin-h5-proxy ./temp --con=test
+
+cmd2app 			-> bin
+port 				-> global option
+new 				-> command
+gomeplusFED			-> command params 1
+./temp 				-> command params 2
+con				-> command option
+`
 		};
 	},
 	ready() {
